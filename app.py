29867cba7af7c478f092537301693cfb6dc4d68f
tusharm03 <muppidi.tushar@gmail.com>
2024-02-22 12:16:38 -0700

    chatgpt pls

--- app.py
+++ app.py
@@ -166,5 +166,6 @@ def all_time():
     return add_recommendations('long_term', playlist_name, num_songs)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
+
     

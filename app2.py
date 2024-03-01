9604d98a7b8d37ce25b9f2517943c14b1dd8db02
tusharm03 <muppidi.tushar@gmail.com>
2024-03-01 11:53:08 -0700

    ;login

--- app2.py
+++ app2.py
@@ -125,7 +125,7 @@ def callback():
 # Route for the main page
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    if not ('access_token' in session and 'user_id' in session):
+    if not ('access_token' in session or 'user_id' in session):
         # Redirect to the login page if not authenticated
         return render_template('login.html')
 
